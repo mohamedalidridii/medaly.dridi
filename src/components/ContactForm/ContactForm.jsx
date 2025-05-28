9b96a430699d80d228107211e3c98f1ade19a59a
Mohamed Ali Dridi <96180127+mohamedalidridii@users.noreply.github.com>
2025-05-28 13:52:32 +0100

    ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -50,10 +50,7 @@ const ContactForm = () => {
           </div> */}
 
           <div className="form-item">
-          <Link to="https://linktr.ee/medaly.dridi" className="btn">
-            Get in Touch &rarr;
-          </Link>
-$          </div>
+          </div>
         </div>
       </div>
     </div>
