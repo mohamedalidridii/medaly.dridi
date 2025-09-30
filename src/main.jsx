32a16204eb75a56a28bb2e9ab6dcdc74785746d8
mohamedalidridii <mohamedali.dridi1@esprit.tn>
2025-10-01 00:32:06 +0100

    fixing metadata

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
+ 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Router>
